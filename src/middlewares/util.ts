bb41d18ae0bd681bde10a0f496b1b2dacbc0e9f2
Dallin <dallin@qloo.com>
2025-01-15 12:26:02 -0700

    customErrors

--- src/middlewares/util.ts
+++ src/middlewares/util.ts
@@ -7,18 +7,26 @@ export class ContentType {
   public readonly charSet: string = null;
   public readonly withoutBoundary: string = null;
   public readonly isWildCard: boolean;
+
   private constructor(contentType: string | null) {
     this.contentType = contentType;
     if (contentType) {
-      this.withoutBoundary = contentType.replace(/;\s{0,}boundary.*/, '').toLowerCase();
-      this.mediaType = this.withoutBoundary.split(';')[0].toLowerCase().trim();
+      this.withoutBoundary = contentType
+        .replace(/;\s{0,}boundary.*/, '')
+        .toLowerCase();
+      this.mediaType = this.withoutBoundary
+        .split(';')[0]
+        .toLowerCase()
+        .trim();
       this.charSet = this.withoutBoundary.split(';')[1]?.toLowerCase();
       this.isWildCard = RegExp(/^[a-z]+\/\*$/).test(this.contentType);
+
       if (this.charSet) {
         this.charSet = this.charSet.toLowerCase().trim();
       }
     }
   }
+
   public static fromString(contentType: string): ContentType {
     return new ContentType(contentType);
   }
@@ -28,7 +36,9 @@ export class ContentType {
   }
 
   public equivalents(): string[] {
-    if (!this.withoutBoundary) {return [];}
+    if (!this.withoutBoundary) {
+      return [];
+    }
     if (this.charSet) {
       return [this.mediaType, `${this.mediaType}; ${this.charSet}`];
     }
@@ -38,7 +48,6 @@ export class ContentType {
 
 /**
  * (side-effecting) modifies the errors object
- * TODO - do this some other way
  * @param errors
  */
 export function augmentAjvErrors(
@@ -55,20 +64,91 @@ export function augmentAjvErrors(
   });
   return errors;
 }
-export function ajvErrorsToValidatorError(errors: ErrorObject[]): ValidationErrorItem[] {
-  return errors.map(e => {
-    const params: any = e.params;
-    const required = params?.missingProperty && `${e.instancePath}.${params.missingProperty}`;
-    const additionalProperty = params?.additionalProperty && `${e.instancePath}.${params.additionalProperty}`;
-    const path = required ?? additionalProperty ?? e.instancePath ?? e.schemaPath;
-    const originalPath = e.instancePath ?? e.schemaPath;
 
-    let fullMessage = `request${path} ${e.message}`;
+function getCustomErrorMessage(e: ErrorObject): string {
+  console.log('util.ts getCustomErrorMessage() e:', e);
+
+  let message: string;
+
+  switch (true) {
+    case e.message.includes('must match pattern "^(-?\\d+(\\.\\d+)?),(-?\\d+(\\.\\d+)?)$"'):
+      message = 'invalid. Location must be in the form lat,lon (e.g. 40.726408,-73.994275)';
+      break;
+
+    case e.message.includes('must match pattern "^external\\.\\w+$"'):
+      message = 'must be a valid external source';
+      break;
+
+    // minimum case for page/take (ex: limit=1)
+    case e.keyword === 'minimum'
+    && e.params.limit === 1
+    && (e.instancePath.includes('.query.take') || e.instancePath.includes('.query.page')):
+      message = 'must be a positive integer';
+      break;
+
+    // maximum case for take (ex:limit=200)
+    case e.keyword === 'maximum'
+    && e.params.limit === 200
+    && e.instancePath.includes('.query.take'):
+      message = 'cannot exceed 1000';
+      break;
+
+    default:
+      message = e.message;
+      break;
+  }
+
+  return message;
+}
+
+export function ajvErrorsToValidatorError(
+  errors: ErrorObject[]
+): ValidationErrorItem[] {
+  return errors.map(e => {
+    console.log('ajvErrorsToValidatorError() e:', e);
+    const params: any = e.params;
+    const required
+      = params?.missingProperty
+      && `${e.instancePath}.${params.missingProperty}`;
+    const additionalProperty
+      = params?.additionalProperty
+      && `${e.instancePath}.${params.additionalProperty}`;
+
+    const path = required
+      ? required
+      : additionalProperty
+        ? additionalProperty
+        : e.instancePath
+          ? e.instancePath
+          : e.schemaPath;
+
+    console.log('e.instancePath:', e.instancePath);
+    const paramNameMatch = e.instancePath.match(/\.query\["(.+?)"\]/);
+    console.log('paramNameMatch:', paramNameMatch);
+    let paramName = paramNameMatch ? paramNameMatch[1] : '';
+    console.log('paramName:', paramName);
+
+    if (!paramName) {
+      paramName = e.instancePath.split('.').slice(2).join('.');
+    }
+    console.log('paramName2:', paramName);
+
+    const originalPath = e.instancePath ?? e.schemaPath;
+    console.log('originalPath:', originalPath);
+    let fullMessage = '';
+
     if (additionalProperty) {
+      console.log('additionalProperty');
       fullMessage = `request${originalPath} must NOT have additional property: '${params.additionalProperty}'`;
     } else if (required) {
+      console.log('required');
       fullMessage = `missing required property request${path}`;
+    } else {
+      console.log('else');
+      const customMessage = getCustomErrorMessage(e);
+      fullMessage = `${paramName} ${customMessage}`;
     }
+
     return {
       path,
       message: e.message,
